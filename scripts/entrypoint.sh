87994bd4403a275956fd5556975590984f347661
k1LoW <k1lowxb@gmail.com>
2022-01-16 16:58:36 +0900

    Support ruby-version using ruby/ruby-builder

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,14 @@ if [ -n "$INPUT_PRE_COMMAND" ]; then
     eval "$INPUT_PRE_COMMAND"
 fi
 
+if [ -n "$RUBY_VERSION"]; then
+    mkdir -p /opt/hostedtoolcache/Ruby/$RUBY_VERSION
+    curl -sL https://github.com/ruby/ruby-builder/releases/download/toolcache/ruby-$RUBY_VERSION-ubuntu-20.04.tar.gz -O /tmp/ruby.tar.gz
+    tar -xz -C /opt/hostedtoolcache/Ruby/$RUBY_VERSION -f /tmp/ruby.tar.gz
+    export PATH=/opt/hostedtoolcache/Ruby/$RUBY_VERSION/x64/bin:$PATH
+    bundle install --gemfile=/github-script-ruby/Gemfile
+fi
+
 if [ -n "$INPUT_GEMFILE" ]; then
     echo "$INPUT_GEMFILE" > /tmp/Gemfile
     cat /tmp/Gemfile
